0857f5d02dfecff2d60d8fecac7326fbc7b845bd
Martin Fraga <martin.fraga@lambdaclass.com>
2024-01-05 18:46:35 -0300

    add ClientError variant for missing fields in rpc api

--- src/errors.rs
+++ src/errors.rs
@@ -23,6 +23,7 @@ pub enum ClientError {
     StoreError(StoreError),
     TransactionExecutionError(TransactionExecutorError),
     TransactionProvingError(TransactionProverError),
+    RpcExpectedFieldMissingFailure(String),
 }
 
 impl fmt::Display for ClientError {
@@ -42,6 +43,9 @@ impl fmt::Display for ClientError {
             }
             ClientError::TransactionProvingError(err) => {
                 write!(f, "transaction prover error: {err}")
+            }
+            ClientError::RpcExpectedFieldMissingFailure(err) => {
+                write!(f, "rpc api error: {err}")
             }
         }
     }
