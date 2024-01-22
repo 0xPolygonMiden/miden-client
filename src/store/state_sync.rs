cc1140df5bce4f500f0231e4574c2d495bae7642
Nacho <amigoigna@gmail.com>
2024-01-22 12:30:59 -0300

    Reviews

--- src/store/state_sync.rs
+++ src/store/state_sync.rs
@@ -111,10 +111,10 @@ impl Store {
                 (account_update.account_id, account_update.account_hash)
             {
                 let account_id_int: u64 = account_id.clone().into();
-                const ACCOUNT_HASH_QUERY: &str = "SELECT id, nonce, vault_root, storage_root, code_root, account_seed FROM accounts WHERE id = ?";
+                const ACCOUNT_QUERY: &str = "SELECT id, nonce, vault_root, storage_root, code_root, account_seed FROM accounts WHERE id = ?";
 
                 if let Some(Ok((acc_stub, _acc_seed))) = tx
-                    .prepare(ACCOUNT_HASH_QUERY)
+                    .prepare(ACCOUNT_QUERY)
                     .map_err(StoreError::QueryError)?
                     .query_map(params![account_id_int as i64], parse_accounts_columns)
                     .map_err(StoreError::QueryError)?
