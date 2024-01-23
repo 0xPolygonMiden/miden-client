29e4c431de520c6f5048b1d3029fecc0eda55bb2
Nacho <amigoigna@gmail.com>
2024-01-23 17:43:12 -0300

    Address reviews

--- src/cli/transactions.rs
+++ src/cli/transactions.rs
@@ -9,20 +9,6 @@ use objects::{accounts::AccountId, assets::FungibleAsset, notes::NoteId, Digest}
 
 use super::{Client, Parser};
 
-#[derive(Debug, Parser, Clone)]
-#[clap(about = "View transactions")]
-pub enum Transaction {
-    /// List transactions
-    #[clap(short_flag = 'l')]
-    List,
-    /// Execute a transaction, prove and submit it to the node
-    #[clap(short_flag = 'n')]
-    New {
-        #[clap(subcommand)]
-        transaction_type: TransactionType,
-    },
-}
-
 #[derive(Clone, Debug, Parser)]
 #[clap()]
 pub enum TransactionType {
@@ -44,6 +30,86 @@ pub enum TransactionType {
     },
 }
 
+impl TryInto<TransactionTemplate> for &TransactionType {
+    type Error = String;
+
+    fn try_into(self) -> Result<TransactionTemplate, Self::Error> {
+        match self {
+            TransactionType::P2ID {
+                sender_account_id,
+                target_account_id,
+                faucet_id,
+                amount,
+            } => {
+                let faucet_id = AccountId::from_hex(faucet_id).map_err(|err| err.to_string())?;
+                let fungible_asset = FungibleAsset::new(faucet_id, *amount)
+                    .map_err(|err| err.to_string())?
+                    .into();
+                let sender_account_id =
+                    AccountId::from_hex(sender_account_id).map_err(|err| err.to_string())?;
+                let target_account_id =
+                    AccountId::from_hex(target_account_id).map_err(|err| err.to_string())?;
+                let payment_transaction = PaymentTransactionData::new(
+                    fungible_asset,
+                    sender_account_id,
+                    target_account_id,
+                );
+
+                Ok(TransactionTemplate::PayToId(payment_transaction))
+            }
+            TransactionType::P2IDR => {
+                todo!()
+            }
+            TransactionType::Mint {
+                faucet_id,
+                target_account_id,
+                amount,
+            } => {
+                let faucet_id = AccountId::from_hex(faucet_id).map_err(|err| err.to_string())?;
+                let fungible_asset =
+                    FungibleAsset::new(faucet_id, *amount).map_err(|err| err.to_string())?;
+                let target_account_id =
+                    AccountId::from_hex(target_account_id).map_err(|err| err.to_string())?;
+
+                Ok(TransactionTemplate::MintFungibleAsset {
+                    asset: fungible_asset,
+                    target_account_id,
+                })
+            }
+            TransactionType::ConsumeNote {
+                account_id,
+                note_id,
+            } => {
+                let account_id = AccountId::from_hex(account_id).map_err(|err| err.to_string())?;
+                let note_id = match note_id {
+                    Some(note_id) => Some(
+                        Digest::try_from(note_id)
+                            .map(NoteId::from)
+                            .map_err(|err| err.to_string())?,
+                    ),
+                    None => None,
+                };
+
+                Ok(TransactionTemplate::ConsumeNote(account_id, note_id))
+            }
+        }
+    }
+}
+
+#[derive(Debug, Parser, Clone)]
+#[clap(about = "View transactions")]
+pub enum Transaction {
+    /// List transactions
+    #[clap(short_flag = 'l')]
+    List,
+    /// Execute a transaction, prove and submit it to the node
+    #[clap(short_flag = 'n')]
+    New {
+        #[clap(subcommand)]
+        transaction_type: TransactionType,
+    },
+}
+
 impl Transaction {
     pub async fn execute(&self, mut client: Client) -> Result<(), String> {
         match self {
@@ -51,67 +117,7 @@ impl Transaction {
                 list_transactions(client)?;
             }
             Transaction::New { transaction_type } => {
-                let transaction_template = match transaction_type {
-                    TransactionType::P2ID {
-                        sender_account_id,
-                        target_account_id,
-                        faucet_id,
-                        amount,
-                    } => {
-                        let faucet_id =
-                            AccountId::from_hex(faucet_id).map_err(|err| err.to_string())?;
-                        let fungible_asset = FungibleAsset::new(faucet_id, *amount)
-                            .map_err(|err| err.to_string())?
-                            .into();
-                        let sender_account_id = AccountId::from_hex(sender_account_id)
-                            .map_err(|err| err.to_string())?;
-                        let target_account_id = AccountId::from_hex(target_account_id)
-                            .map_err(|err| err.to_string())?;
-                        let payment_transaction = PaymentTransactionData::new(
-                            fungible_asset,
-                            sender_account_id,
-                            target_account_id,
-                        );
-
-                        TransactionTemplate::PayToId(payment_transaction)
-                    }
-                    TransactionType::P2IDR => {
-                        todo!()
-                    }
-                    TransactionType::Mint {
-                        faucet_id,
-                        target_account_id,
-                        amount,
-                    } => {
-                        let faucet_id =
-                            AccountId::from_hex(faucet_id).map_err(|err| err.to_string())?;
-                        let fungible_asset = FungibleAsset::new(faucet_id, *amount)
-                            .map_err(|err| err.to_string())?;
-                        let target_account_id = AccountId::from_hex(target_account_id)
-                            .map_err(|err| err.to_string())?;
-                        TransactionTemplate::MintFungibleAsset {
-                            asset: fungible_asset,
-                            target_account_id,
-                        }
-                    }
-                    TransactionType::ConsumeNote {
-                        account_id,
-                        note_id,
-                    } => {
-                        let account_id =
-                            AccountId::from_hex(account_id).map_err(|err| err.to_string())?;
-                        let note_id = match note_id {
-                            Some(note_id) => Some(
-                                Digest::try_from(note_id)
-                                    .map(NoteId::from)
-                                    .map_err(|err| err.to_string())?,
-                            ),
-                            None => None,
-                        };
-
-                        TransactionTemplate::ConsumeNote(account_id, note_id)
-                    }
-                };
+                let transaction_template: TransactionTemplate = transaction_type.try_into()?;
 
                 let transaction_execution_result = client
                     .new_transaction(transaction_template.clone())
--- src/client/sync_state.rs
+++ src/client/sync_state.rs
@@ -107,8 +107,7 @@ impl Client {
         .map_err(ClientError::StoreError)?;
 
         tx.commit()
-            .map_err(|err| ClientError::StoreError(StoreError::TransactionError(err)))?;
-        Ok(())
+            .map_err(|err| ClientError::StoreError(StoreError::TransactionError(err)))
     }
 
     async fn single_sync_state(&mut self) -> Result<SyncStatus, ClientError> {
