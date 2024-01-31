19c2e58dca3c5a66396355e5426dd1912636869f
Martin Fraga <fragamartine@gmail.com>
2024-01-31 20:17:36 -0300

    rename request for display impl

--- src/client/rpc_client.rs
+++ src/client/rpc_client.rs
@@ -100,7 +100,7 @@ impl fmt::Display for RpcApiEndpoint {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             RpcApiEndpoint::GetBlockHeaderByNumber => write!(f, "get_block_header_by_number"),
-            RpcApiEndpoint::SyncState => write!(f, "state_sync"),
+            RpcApiEndpoint::SyncState => write!(f, "sync_state"),
             RpcApiEndpoint::SubmitProvenTx => write!(f, "submit_proven_transaction"),
         }
     }
