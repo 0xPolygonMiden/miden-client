03f939494515b9ec59ade50db56f83f762102e96
igamigo <ignacio.amigo@lambdaclass.com>
2024-02-01 17:02:17 -0300

    Remove cli/sync_state.rs (#137)

--- src/cli/sync.rs
+++ src/cli/sync.rs
@@ -2,6 +2,6 @@ use miden_client::client::Client;
 
 pub async fn sync_state(mut client: Client) -> Result<(), String> {
     let block_num = client.sync_state().await.map_err(|e| e.to_string())?;
-    println!("state synced to block {}", block_num);
+    println!("State synced to block {}", block_num);
     Ok(())
 }
