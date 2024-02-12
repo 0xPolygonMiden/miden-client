0848340c5903413c7f0a73be5c2c75b1347fe0fd
Paul-Henry Kajfasz <kajfaszph@gmail.com>
2024-02-12 13:44:35 +0100

    Revert "Remove _t for clippy"
    This reverts commit f88af4c6cf9870f69a177351ca274c3767a804ac.

--- src/cli/account.rs
+++ src/cli/account.rs
@@ -32,13 +32,13 @@ pub enum AccountCmd {
         // TODO: We should create a value parser for catching input parsing errors earlier (ie AccountID) once complexity grows
         #[clap()]
         id: Option<String>,
-        #[clap(short, long, default_value = false)]
+        #[clap(short, long, default_value_t = false)]
         keys: bool,
-        #[clap(short, long, default_value = false)]
+        #[clap(short, long, default_value_t = false)]
         vault: bool,
-        #[clap(short, long, default_value = false)]
+        #[clap(short, long, default_value_t = false)]
         storage: bool,
-        #[clap(short, long, default_value = false)]
+        #[clap(short, long, default_value_t = false)]
         code: bool,
     },
     /// Create new account and store it locally
