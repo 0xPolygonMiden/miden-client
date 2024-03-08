37d8e97a592cb1d35e202b6ce0d19269dcddbde4
Martin Fraga <martin.fraga@lambdaclass.com>
2024-03-08 13:34:46 -0300

    add p2idr tx

--- src/client/transactions.rs
+++ src/client/transactions.rs
@@ -394,6 +394,54 @@ impl<N: NodeRpcClient, S: Store> Client<N, S> {
         )
     }
 
+    fn new_p2idr_transaction(
+        &mut self,
+        fungible_asset: Asset,
+        sender_account_id: AccountId,
+        target_account_id: AccountId,
+        recall_height: u32,
+    ) -> Result<TransactionResult, ClientError> {
+        let random_coin = self.get_random_coin();
+
+        let created_note = create_p2idr_note(
+            sender_account_id,
+            target_account_id,
+            vec![fungible_asset],
+            recall_height,
+            random_coin,
+        )?;
+
+        self.tx_executor.load_account(sender_account_id)?;
+
+        let block_ref = self.get_sync_height()?;
+
+        let recipient = created_note
+            .recipient()
+            .iter()
+            .map(|x| x.as_int().to_string())
+            .collect::<Vec<_>>()
+            .join(".");
+
+        let tx_script_code = ProgramAst::parse(
+            &AUTH_SEND_ASSET_SCRIPT
+                .replace("{recipient}", &recipient)
+                .replace(
+                    "{tag}",
+                    &Felt::new(Into::<u64>::into(target_account_id)).to_string(),
+                )
+                .replace("{asset}", &prepare_word(&fungible_asset.into()).to_string()),
+        )
+        .expect("shipped MASM is well-formed");
+
+        self.compile_and_execute_tx(
+            sender_account_id,
+            &[],
+            vec![created_note],
+            tx_script_code,
+            block_ref,
+        )
+    }
+
     fn compile_and_execute_tx(
         &mut self,
         account_id: AccountId,
