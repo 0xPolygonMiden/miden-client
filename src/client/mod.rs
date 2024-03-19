a3915e064310cbdd2dfb4d0e1230d222e36dbe75
Martin Fraga <martin.fraga@lambdaclass.com>
2024-03-19 16:53:31 -0300

    add wrapper trait to randomly generate seeds

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -22,6 +22,24 @@ use crate::mock::MockDataStore;
 #[cfg(not(any(test, feature = "mock")))]
 use crate::store::data_store::ClientDataStore;
 
+// CLIENT RNG
+// ================================================================================================
+pub trait ClientRng: FeltRng {
+    fn get_random_seed(&mut self) -> [u8; 32] {
+        let mut seed = [0; 32];
+        let word = self.draw_word();
+
+        seed[0..8].copy_from_slice(&word[0].inner().to_le_bytes());
+        seed[8..16].copy_from_slice(&word[1].inner().to_le_bytes());
+        seed[16..24].copy_from_slice(&word[2].inner().to_le_bytes());
+        seed[24..32].copy_from_slice(&word[3].inner().to_le_bytes());
+
+        seed
+    }
+}
+
+impl<T: FeltRng> ClientRng for T {}
+
 // MIDEN CLIENT
 // ================================================================================================
 
@@ -33,10 +51,10 @@ use crate::store::data_store::ClientDataStore;
 /// - Connects to one or more Miden nodes to periodically sync with the current state of the
 ///   network.
 /// - Executes, proves, and submits transactions to the network as directed by the user.
-pub struct Client<N: NodeRpcClient, R: FeltRng, S: Store> {
+pub struct Client<N: NodeRpcClient, R: ClientRng, S: Store> {
     /// The client's store, which provides a way to write and read entities to provide persistence.
     store: S,
-    /// An instance of [FeltRng] which provides randomness tools for generating new keys, serial numbers, etc.
+    /// An instance of [ClientRng] which provides randomness tools for generating new keys, serial numbers, etc.
     rng: R,
     /// An instance of [NodeRpcClient] which provides a way for the client to connect to the Miden node.
     rpc_api: N,
@@ -46,7 +64,7 @@ pub struct Client<N: NodeRpcClient, R: FeltRng, S: Store> {
     tx_executor: TransactionExecutor<MockDataStore>,
 }
 
-impl<N: NodeRpcClient, R: FeltRng, S: Store> Client<N, R, S> {
+impl<N: NodeRpcClient, R: ClientRng, S: Store> Client<N, R, S> {
     // CONSTRUCTOR
     // --------------------------------------------------------------------------------------------
 
