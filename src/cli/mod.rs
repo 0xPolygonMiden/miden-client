d9f3a45c3eec7898fddc24fd6e94ba141e2a40e8
Martin Fraga <fragamartine@gmail.com>
2024-04-30 16:56:30 -0300

    feat: add timeout to rpc config (#298)
    * feat: add timeout to rpc config
    
    * docs: mention new setting in documentation
    
    * fix: add missing config field for integration test and set default serde value
    
    * update CHANGELOG
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: igamigo <ignacio.amigo@lambdaclass.com>

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -87,7 +87,6 @@ impl Cli {
 
         // Create the client
         let client_config = load_config(current_dir.as_path())?;
-        let rpc_endpoint = client_config.rpc.endpoint.to_string();
         let store = SqliteStore::new((&client_config).into()).map_err(ClientError::StoreError)?;
         let rng = get_random_coin();
         let executor_store =
@@ -95,7 +94,7 @@ impl Cli {
                 .map_err(ClientError::StoreError)?;
 
         let client: Client<TonicRpcClient, RpoRandomCoin, SqliteStore> = Client::new(
-            TonicRpcClient::new(&rpc_endpoint),
+            TonicRpcClient::new(&client_config.rpc),
             rng,
             store,
             executor_store,
