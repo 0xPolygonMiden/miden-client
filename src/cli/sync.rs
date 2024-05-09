dd21a15a42a2b428495ccc9ce2a398154b399238
Tomas Rodriguez Dala <43424983+tomyrd@users.noreply.github.com>
2024-05-09 12:39:59 -0300

    feat: add better feedback to multiple commands (#323)
    * feat: export notes from output notes table (converted to input notes)
    
    * refactor: rewrite test to display new behavior
    
    * Add note details command when importing note
    
    * Add account details command when creating new account
    
    * Erase TODO, fixed in PR #297
    
    * Add more feedback to `tx new` command
    
    * feat: search both input and output notes when looking for notes
    
    * refactor: rename input_notes file to notes
    
    * Add more information to `SyncStatus`
    
    * Add tests for new sync details
    
    * Add changes to CHANGELOG
    
    * Rename `SyncNewDetails` and add docs
    
    * address review comments
    
    * fix: handle corner cases on show command
    
    * Fix suggestions
    
    * Remove unnecessary message
    
    * Move binary name to constant
    
    * Move binary name inline
    
    * Change `add` to `combine_with`
    
    * Change transaction output notes message
    
    ---------
    
    Co-authored-by: Martin Fraga <martin.fraga@lambdaclass.com>

--- src/cli/sync.rs
+++ src/cli/sync.rs
@@ -7,7 +7,12 @@ use miden_objects::crypto::rand::FeltRng;
 pub async fn sync_state<N: NodeRpcClient, R: FeltRng, S: Store>(
     mut client: Client<N, R, S>,
 ) -> Result<(), String> {
-    let block_num = client.sync_state().await?;
-    println!("State synced to block {}", block_num);
+    let new_details = client.sync_state().await?;
+    println!("State synced to block {}", new_details.block_num);
+    println!("New public notes: {}", new_details.new_notes);
+    println!("Tracked notes updated: {}", new_details.new_inclusion_proofs);
+    println!("Tracked notes consumed: {}", new_details.new_nullifiers);
+    println!("Tracked accounts updated: {}", new_details.updated_onchain_accounts);
+    println!("Commited transactions: {}", new_details.commited_transactions);
     Ok(())
 }
