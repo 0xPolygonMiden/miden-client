ea53b1aa9761020f2731b78eefe743825df0a6d6
Martin Fraga <martin.fraga@lambdaclass.com>
2024-05-13 19:06:22 -0300

    test: add test for offchain swap flow

--- tests/integration/swap_transactions_tests.rs
+++ tests/integration/swap_transactions_tests.rs
@@ -206,6 +206,196 @@ async fn test_swap_fully_onchain() {
     }
 }
 
+#[tokio::test]
+#[ignore = "Currently we're unable to import offchain notes from blocks we are not tracking. Once https://github.com/0xPolygonMiden/miden-client/pull/337 gets merged we should be able to remove this ignore."]
+async fn test_swap_offchain() {
+    const OFFERED_ASSET_AMOUNT: u64 = 1;
+    const REQUESTED_ASSET_AMOUNT: u64 = 25;
+    let mut client1 = create_test_client();
+    wait_for_node(&mut client1).await;
+    let mut client2 = create_test_client();
+    let mut client_with_faucets = create_test_client();
+
+    client1.sync_state().await.unwrap();
+    client2.sync_state().await.unwrap();
+    client_with_faucets.sync_state().await.unwrap();
+
+    // Create Client 1's basic wallet (We'll call it accountA)
+    let (account_a, _) = client1
+        .new_account(AccountTemplate::BasicWallet {
+            mutable_code: false,
+            storage_mode: AccountStorageMode::Local,
+        })
+        .unwrap();
+
+    // Create Client 2's basic wallet (We'll call it accountB)
+    let (account_b, _) = client2
+        .new_account(AccountTemplate::BasicWallet {
+            mutable_code: false,
+            storage_mode: AccountStorageMode::Local,
+        })
+        .unwrap();
+
+    // Create client with faucets BTC faucet (note: it's not real BTC)
+    let (btc_faucet_account, _) = client_with_faucets
+        .new_account(AccountTemplate::FungibleFaucet {
+            token_symbol: TokenSymbol::new("BTC").unwrap(),
+            decimals: 8,
+            max_supply: 1_000_000,
+            storage_mode: AccountStorageMode::Local,
+        })
+        .unwrap();
+    // Create client with faucets ETH faucet (note: it's not real ETH)
+    let (eth_faucet_account, _) = client_with_faucets
+        .new_account(AccountTemplate::FungibleFaucet {
+            token_symbol: TokenSymbol::new("ETH").unwrap(),
+            decimals: 8,
+            max_supply: 1_000_000,
+            storage_mode: AccountStorageMode::Local,
+        })
+        .unwrap();
+
+    // mint 1000 BTC for accountA
+    mint_note(
+        &mut client_with_faucets,
+        account_a.id(),
+        btc_faucet_account.id(),
+        NoteType::Public,
+    )
+    .await;
+    // mint 1000 ETH for accountB
+    mint_note(
+        &mut client_with_faucets,
+        account_b.id(),
+        btc_faucet_account.id(),
+        NoteType::Public,
+    )
+    .await;
+
+    // Sync and consume note for accountA
+    client1.sync_state().await.unwrap();
+    let client_1_notes = client1.get_input_notes(miden_client::store::NoteFilter::All).unwrap();
+    assert_eq!(client_1_notes.len(), 1);
+
+    println!("Consuming mint note on first client...");
+    let tx_template =
+        TransactionTemplate::ConsumeNotes(account_a.id(), vec![client_1_notes[0].id()]);
+    let tx_request = client1.build_transaction_request(tx_template).unwrap();
+    execute_tx_and_sync(&mut client1, tx_request).await;
+
+    // Sync and consume note for accountB
+    client2.sync_state().await.unwrap();
+    let client_2_notes = client2.get_input_notes(miden_client::store::NoteFilter::All).unwrap();
+    assert_eq!(client_2_notes.len(), 1);
+
+    println!("Consuming mint note on second client...");
+    let tx_template =
+        TransactionTemplate::ConsumeNotes(account_b.id(), vec![client_2_notes[0].id()]);
+    let tx_request = client2.build_transaction_request(tx_template).unwrap();
+    execute_tx_and_sync(&mut client2, tx_request).await;
+
+    // Create ONCHAIN swap note (clientA offers 1 BTC in exchange of 25 ETH)
+    // check that account now has 1 less BTC
+    let offered_asset = FungibleAsset::new(btc_faucet_account.id(), OFFERED_ASSET_AMOUNT).unwrap();
+    let requested_asset =
+        FungibleAsset::new(eth_faucet_account.id(), REQUESTED_ASSET_AMOUNT).unwrap();
+    let tx_template = TransactionTemplate::Swap(
+        SwapTransactionData::new(
+            account_a.id(),
+            Asset::Fungible(offered_asset),
+            Asset::Fungible(requested_asset),
+        ),
+        NoteType::OffChain,
+    );
+    println!("Running SWAP tx...");
+    let tx_request = client1.build_transaction_request(tx_template).unwrap();
+
+    let expected_output_notes = tx_request.expected_output_notes().to_vec();
+    let expected_payback_note_details = tx_request.expected_payback_note_details().to_vec();
+    assert_eq!(expected_output_notes.len(), 1);
+    assert_eq!(expected_payback_note_details.len(), 1);
+
+    execute_tx_and_sync(&mut client1, tx_request).await;
+
+    // Export note from client 1 to client 2
+    let exported_note = client1.get_output_note(expected_output_notes[0].id()).unwrap();
+    client2
+        .import_input_note(exported_note.try_into().unwrap(), true)
+        .await
+        .unwrap();
+
+    // consume swap note with accountB, and check that the vault changed appropiately
+    println!("Consuming swap note on second client...");
+    let tx_template =
+        TransactionTemplate::ConsumeNotes(account_b.id(), vec![expected_output_notes[0].id()]);
+    let tx_request = client2.build_transaction_request(tx_template).unwrap();
+    execute_tx_and_sync(&mut client2, tx_request).await;
+
+    // sync on client 1, we should get the missing payback note details.
+    // try consuming the received note with accountA, it should now have 25 ETH
+    client1.sync_state().await.unwrap();
+    println!("Consuming swap payback note on first client...");
+    let tx_template = TransactionTemplate::ConsumeNotes(
+        account_a.id(),
+        vec![expected_payback_note_details[0].id()],
+    );
+    let tx_request = client1.build_transaction_request(tx_template).unwrap();
+    execute_tx_and_sync(&mut client1, tx_request).await;
+
+    // At the end we should end up with
+    //
+    // - accountA: 999 BTC, 25 ETH
+    // - accountB: 1 BTC, 975 ETH
+
+    // first reload the account
+    let (account_a, _) = client1.get_account(account_a.id()).unwrap();
+    assert_eq!(account_a.vault().assets().count(), 2);
+
+    let asset_1 = account_a.vault().assets().next().unwrap();
+    let asset_2 = account_a.vault().assets().next().unwrap();
+
+    match (asset_1, asset_2) {
+        (Asset::Fungible(btc_asset), Asset::Fungible(eth_asset))
+            if btc_asset.faucet_id() == btc_faucet_account.id()
+                && eth_asset.faucet_id() == eth_faucet_account.id() =>
+        {
+            assert_eq!(btc_asset.amount(), 999);
+            assert_eq!(eth_asset.amount(), 25);
+        },
+        (Asset::Fungible(eth_asset), Asset::Fungible(btc_asset))
+            if btc_asset.faucet_id() == btc_faucet_account.id()
+                && eth_asset.faucet_id() == eth_faucet_account.id() =>
+        {
+            assert_eq!(btc_asset.amount(), 999);
+            assert_eq!(eth_asset.amount(), 25);
+        },
+        _ => panic!("should only have fungible assets!"),
+    }
+
+    let (account_b, _) = client2.get_account(account_b.id()).unwrap();
+
+    let asset_1 = account_b.vault().assets().next().unwrap();
+    let asset_2 = account_b.vault().assets().next().unwrap();
+
+    match (asset_1, asset_2) {
+        (Asset::Fungible(btc_asset), Asset::Fungible(eth_asset))
+            if btc_asset.faucet_id() == btc_faucet_account.id()
+                && eth_asset.faucet_id() == eth_faucet_account.id() =>
+        {
+            assert_eq!(btc_asset.amount(), 1);
+            assert_eq!(eth_asset.amount(), 975);
+        },
+        (Asset::Fungible(eth_asset), Asset::Fungible(btc_asset))
+            if btc_asset.faucet_id() == btc_faucet_account.id()
+                && eth_asset.faucet_id() == eth_faucet_account.id() =>
+        {
+            assert_eq!(btc_asset.amount(), 1);
+            assert_eq!(eth_asset.amount(), 975);
+        },
+        _ => panic!("should only have fungible assets!"),
+    }
+}
+
 /// Returns a note tag for a swap note with the specified parameters.
 ///
 /// Use case ID for the returned tag is set to 0.
