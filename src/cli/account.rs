b0d69b67bfebbbee52ed51bf91944dead0e9afb9
Tomas Rodriguez Dala <43424983+tomyrd@users.noreply.github.com>
2024-05-24 16:02:06 -0300

    refactor: simplify and separate note list (#356)
    * Reduce and split notes list
    
    * Fix bug in `update_note_consumer_tx_id`
    
    * Simplify account list
    
    * Add change to CHANGELOG
    
    * Add storage type to account list
    
    * Change styling

--- src/cli/account.rs
+++ src/cli/account.rs
@@ -110,21 +110,10 @@ fn list_accounts<N: NodeRpcClient, R: FeltRng, S: Store, A: TransactionAuthentic
 ) -> Result<(), String> {
     let accounts = client.get_account_stubs()?;
 
-    let mut table = create_dynamic_table(&[
-        "Account ID",
-        "Code Root",
-        "Vault Root",
-        "Storage Root",
-        "Type",
-        "Storage mode",
-        "Nonce",
-    ]);
+    let mut table = create_dynamic_table(&["Account ID", "Type", "Storage Mode", "Nonce"]);
     accounts.iter().for_each(|(acc, _acc_seed)| {
         table.add_row(vec![
             acc.id().to_string(),
-            acc.code_root().to_string(),
-            acc.vault_root().to_string(),
-            acc.storage_root().to_string(),
             account_type_display_name(&acc.id().account_type()),
             storage_type_display_name(&acc.id()),
             acc.nonce().as_int().to_string(),
