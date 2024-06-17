81c85199bcf52ebd6db4b9eb97b170368b271c8e
Ignacio Amigo <ignacio.amigo@lambdaclass.com>
2024-06-17 16:27:04 -0300

    Reviews

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-#[cfg(feature = "executable")]
-use clap::Parser;
-
 extern crate alloc;
-#[cfg(feature = "executable")]
+
 mod cli;
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
     #[cfg(feature = "executable")]
     {
+        use clap::Parser;
+
         tracing_subscriber::fmt::init();
         // read command-line args
         let cli = cli::Cli::parse();
