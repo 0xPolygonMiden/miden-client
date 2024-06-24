7d29e926f39cab4b81ebe33e958632262a7775da
Martin Fraga <martin.fraga@lambdaclass.com>
2024-06-24 12:02:06 -0300

    refactor: add refactors from miden-objects and miden-tx

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,23 @@ pub mod config;
 pub mod errors;
 pub mod store;
 
+/// Miden Base re-exports
+pub mod objects {
+    pub use miden_objects::{
+        accounts::{Account, AccountData, AccountId, AccountStorageType, AuthSecretKey},
+        assembly::ProgramAst,
+        assets::{Asset, FungibleAsset, TokenSymbol},
+        crypto::merkle::{InOrderIndex, MerklePath, MmrDelta, MmrPeaks, MmrProof},
+        notes::{NoteId, NoteMetadata, NoteScript, NoteTag, NoteType},
+        transaction::{ProvenTransaction, TransactionId},
+        BlockHeader, Digest, Felt, Word,
+    };
+}
+
+pub mod tx {
+    pub use miden_tx::{auth::TransactionAuthenticator, ScriptTarget};
+}
+
 #[cfg(all(test, feature = "executable"))]
 pub mod mock;
 
