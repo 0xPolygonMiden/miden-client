9acecbefcf8d92246cc37ee19bfa4b214c2e6046
Martin Fraga <martin.fraga@lambdaclass.com>
2024-06-24 16:48:15 -0300

    refactor: flatten re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -3,37 +3,20 @@ extern crate alloc;
 mod client;
 pub use client::{
     accounts::AccountTemplate, rpc, store_authenticator::StoreAuthenticator, sync::SyncSummary,
-    transactions, Client, NoteConsumability, NoteRelevance,
+    Client, NoteConsumability, NoteRelevance,
 };
 
 pub mod config;
 pub mod errors;
 pub mod store;
 
-/// Miden Base re-exports
-pub mod objects {
-    pub use miden_objects::{
-        accounts::{
-            Account, AccountCode, AccountData, AccountId, AccountStorage, AccountStorageType,
-            AccountStub, AuthSecretKey,
-        },
-        assembly::{AstSerdeOptions, ModuleAst, ProgramAst},
-        assets::{Asset, AssetVault, FungibleAsset, TokenSymbol},
-        crypto::{
-            merkle::{
-                InOrderIndex, LeafIndex, MerklePath, MmrDelta, MmrPeaks, MmrProof, SmtLeaf,
-                SmtProof,
-            },
-            rand::{FeltRng, RpoRandomCoin},
-        },
-        notes::{
-            Note, NoteAssets, NoteExecutionHint, NoteId, NoteInclusionProof, NoteInputs,
-            NoteMetadata, NoteRecipient, NoteScript, NoteTag, NoteType, Nullifier,
-        },
-        transaction::{
-            InputNote, OutputNotes, ProvenTransaction, TransactionId, TransactionScript,
-        },
-        BlockHeader, Digest, Felt, StarkField, Word,
+// MIDEN BASE RE-EXPORTS
+// ================================================================================================
+
+pub mod accounts {
+    pub use miden_objects::accounts::{
+        Account, AccountCode, AccountData, AccountId, AccountStorage, AccountStorageType,
+        AccountStub, AuthSecretKey,
     };
 
     #[cfg(feature = "testing")]
@@ -42,12 +25,44 @@ pub mod objects {
     }
 }
 
-pub mod tx {
+pub mod assembly {
+    pub use miden_objects::assembly::{AstSerdeOptions, ModuleAst, ProgramAst};
+}
+
+pub mod assets {
+    pub use miden_objects::assets::{Asset, AssetVault, FungibleAsset, TokenSymbol};
+}
+
+pub mod crypto {
+    pub use miden_objects::crypto::{
+        merkle::{
+            InOrderIndex, LeafIndex, MerklePath, MmrDelta, MmrPeaks, MmrProof, SmtLeaf, SmtProof,
+        },
+        rand::{FeltRng, RpoRandomCoin},
+    };
+}
+
+pub mod notes {
+    pub use miden_objects::notes::{
+        Note, NoteAssets, NoteExecutionHint, NoteId, NoteInclusionProof, NoteInputs, NoteMetadata,
+        NoteRecipient, NoteScript, NoteTag, NoteType, Nullifier,
+    };
+}
+
+pub use miden_objects::{BlockHeader, Digest, Felt, StarkField, Word};
+
+pub mod transactions {
+    pub use miden_objects::transaction::{
+        ExecutedTransaction, InputNote, OutputNote, OutputNotes, ProvenTransaction, TransactionId,
+        TransactionScript,
+    };
     pub use miden_tx::{
         auth::TransactionAuthenticator,
         utils::{Deserializable, Serializable},
         DataStoreError, ScriptTarget, TransactionExecutorError,
     };
+
+    pub use super::client::transactions::*;
 }
 
 #[cfg(all(test, feature = "executable"))]
