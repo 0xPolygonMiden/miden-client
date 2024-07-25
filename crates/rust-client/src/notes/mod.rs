d58180b18e920a4eb186ef42f634e03e7c32cba2
tomyrd <chubyrd1@gmail.com>
2024-07-25 18:43:04 -0300

    fix: remove unwraps

--- crates/rust-client/src/notes/mod.rs
+++ crates/rust-client/src/notes/mod.rs
@@ -181,7 +181,7 @@ impl<N: NodeRpcClient, R: FeltRng, S: Store, A: TransactionAuthenticator> Client
                             >> FILTER_ID_SHIFT)
                             as u16)])
                         .await
-                        .unwrap();
+                        .map_err(ClientError::RpcError)?;
 
                     let status = if let Some((_, block_num)) =
                         nullifiers.iter().find(|(nullifier, _)| nullifier == &node_note.nullifier())
@@ -298,7 +298,7 @@ impl<N: NodeRpcClient, R: FeltRng, S: Store, A: TransactionAuthenticator> Client
                         >> FILTER_ID_SHIFT)
                         as u16)])
                     .await
-                    .unwrap();
+                    .map_err(ClientError::RpcError)?;
 
                 let status = if let Some((_, block_num)) =
                     nullifiers.iter().find(|(nullifier, _)| nullifier == &note.nullifier())
