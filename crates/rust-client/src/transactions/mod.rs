65679d5663878e337fe9338a91882bc02406351e
SantiagoPittella <pittellasantiago@gmail.com>
2024-08-01 16:20:26 -0300

    avoid vec, use getters for assets

--- crates/rust-client/src/transactions/mod.rs
+++ crates/rust-client/src/transactions/mod.rs
@@ -382,11 +382,11 @@ impl<N: NodeRpcClient, R: FeltRng, S: Store, A: TransactionAuthenticator> Client
         transaction_request: &TransactionRequest,
     ) -> (BTreeMap<AccountId, u64>, BTreeSet<NonFungibleAsset>) {
         // Get own notes assets
-        let own_notes_assets = match transaction_request.script_template() {
+        let mut own_notes_assets = match transaction_request.script_template() {
             Some(TransactionScriptTemplate::SendNotes(notes)) => {
-                notes.iter().map(|note| (note.id(), note.assets())).collect::<Vec<_>>()
+                notes.iter().map(|note| (note.id(), note.assets())).collect::<BTreeMap<_, _>>()
             },
-            _ => vec![],
+            _ => Default::default(),
         };
         // Get transaction output notes assets
         let mut output_notes_assets = transaction_request
@@ -395,7 +395,7 @@ impl<N: NodeRpcClient, R: FeltRng, S: Store, A: TransactionAuthenticator> Client
             .collect::<BTreeMap<_, _>>();
 
         // Merge with own notes assets and delete duplicates
-        output_notes_assets.append(&mut BTreeMap::from_iter(own_notes_assets));
+        output_notes_assets.append(&mut own_notes_assets);
 
         let mut fungible_balance_map: BTreeMap<AccountId, u64> = BTreeMap::new();
         let mut non_fungible_set: BTreeSet<NonFungibleAsset> = BTreeSet::new();
@@ -429,31 +429,35 @@ impl<N: NodeRpcClient, R: FeltRng, S: Store, A: TransactionAuthenticator> Client
 
         // Get client assets
         let (account, _) = maybe_await!(self.get_account(transaction_request.account_id()))?;
-        let mut account_assets = account.vault().assets();
 
-        // Check if the fungible assets are less than or equal to the account assets
+        // Check if the fungible assets are less than or equal to the account asset
         for (faucet_id, amount) in fungible_balance_map {
-            if let Some(account_asset) = account_assets.find(|account_asset| match account_asset {
-                Fungible(account_fungible) => account_fungible.faucet_id() == faucet_id,
-                NonFungible(_) => false,
-            }) {
-                let account_asset_amount = account_asset.unwrap_fungible().amount();
-                if account_asset_amount < amount {
+            match account.vault().get_balance(faucet_id) {
+                Ok(account_asset_amount) => {
+                    if account_asset_amount < amount {
+                        return Err(ClientError::AssetError(AssetError::AssetAmountNotSufficient(
+                            account_asset_amount,
+                            amount,
+                        )));
+                    }
+                },
+                _ => {
                     return Err(ClientError::AssetError(AssetError::AssetAmountNotSufficient(
-                        account_asset_amount,
-                        amount,
-                    )));
-                }
+                        0, amount,
+                    )))
+                },
             }
         }
 
         // Check if the non fungible assets are less than or equal to the account assets
         for non_fungible in non_fungible_set {
-            if !account_assets.any(|asset| match asset {
-                Fungible(_) => false,
-                NonFungible(account_non_fungible) => account_non_fungible == non_fungible,
-            }) {
-                return Err(ClientError::AssetError(AssetError::AssetAmountNotSufficient(1, 0)));
+            match account.vault().has_non_fungible_asset(non_fungible.into()) {
+                Ok(true) => (),
+                _ => {
+                    return Err(ClientError::AssetError(AssetError::AssetAmountNotSufficient(
+                        0, 1,
+                    )));
+                },
             }
         }
 
