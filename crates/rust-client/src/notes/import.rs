2b1284cfea75551def158c1c692181dd37910b9b
tomyrd <chubyrd1@gmail.com>
2024-08-27 17:36:30 -0300

    fix: check note block number after its found

--- crates/rust-client/src/notes/import.rs
+++ crates/rust-client/src/notes/import.rs
@@ -267,6 +267,16 @@ impl<N: NodeRpcClient, R: FeltRng, S: Store, A: TransactionAuthenticator> Client
                 // Therefore, we should mark the note as committed.
                 let note_block_num = sync_notes.block_header.block_num();
 
+                if note_block_num > current_block_num {
+                    return Ok((
+                        NoteStatus::Expected {
+                            created_at: None,
+                            block_height: Some(request_block_num),
+                        },
+                        None,
+                    ));
+                };
+
                 let note_inclusion_proof = NoteInclusionProof::new(
                     note_block_num,
                     note.note_index(),
