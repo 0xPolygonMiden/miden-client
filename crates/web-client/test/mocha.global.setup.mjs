af79f9f545f1ad8a4d65819fbc3b72de58635f9c
Julian <julian@demoxlabs.xyz>
2024-08-28 14:21:59 -0500

    Fixing generated responses in rust client & cleaning up web client test code per comments

--- crates/web-client/test/mocha.global.setup.mjs
+++ crates/web-client/test/mocha.global.setup.mjs
@@ -11,34 +11,36 @@ let serverProcess;
 let browser;
 let testingPage;
 
-const LOCAL_SERVER_PORT = 8080;
-const LOCAL_SERVER = `http://localhost:${LOCAL_SERVER_PORT}`;
+const TEST_SERVER_PORT = 8080;
+const TEST_SERVER = `http://localhost:${TEST_SERVER_PORT}`;
 
-before(async function () {
+// Should be in sync with the rpc port in tests/config/miden-node.toml
+const LOCAL_MIDEN_NODE_PORT = 57291;
+
+before(async () => {
   console.log("Starting test server...");
-  serverProcess = exec(`http-server ./dist -p ${LOCAL_SERVER_PORT}`);
+  serverProcess = exec(`http-server ./dist -p ${TEST_SERVER_PORT}`);
   browser = await puppeteer.launch({ headless: true });
   testingPage = await browser.newPage();
-  await testingPage.goto(LOCAL_SERVER);
+  await testingPage.goto(TEST_SERVER);
 
   // Uncomment below to enable console logging
   // testingPage.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
 
   // Creates the client in the test context and attach to window object
   await testingPage.exposeFunction("create_client", async () => {
-    await testingPage.evaluate(async () => {
+    await testingPage.evaluate(async (port) => {
       const { WebClient } = await import("./index.js");
-      // let rpc_url = "http://18.203.155.106:57291";
-      let rpc_url = "http://localhost:57291";
+      let rpc_url = `http://localhost:${port}`;
       const client = new WebClient();
       await client.create_client(rpc_url);
 
       window.client = client;
-    });
+    }, LOCAL_MIDEN_NODE_PORT);
   });
 });
 
-after(async function () {
+after(async () => {
   console.log("Stopping test server...");
   await browser.close();
   serverProcess.kill();
