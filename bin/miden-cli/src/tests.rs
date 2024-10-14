4aaae8608235c00be406709c64f71b4b665926ea
Ignacio Amigo <ignacio.amigo@lambdaclass.com>
2024-10-14 10:46:10 -0300

    feat: Add tx prover component

--- bin/miden-cli/src/tests.rs
+++ bin/miden-cli/src/tests.rs
@@ -2,21 +2,15 @@ use std::{
     env::{self, temp_dir},
     fs::File,
     io::Read,
-    path::Path,
+    path::Path, sync::Arc,
 };
 
 use assert_cmd::Command;
 use miden_client::{
-    accounts::{Account, AccountId, AccountStorageMode, AccountTemplate},
-    config::RpcConfig,
-    crypto::RpoRandomCoin,
-    rpc::TonicRpcClient,
-    store::{
+    accounts::{Account, AccountId, AccountStorageMode, AccountTemplate}, config::RpcConfig, crypto::RpoRandomCoin, rpc::TonicRpcClient, store::{
         sqlite_store::{config::SqliteStoreConfig, SqliteStore},
         NoteFilter, StoreAuthenticator,
-    },
-    testing::ACCOUNT_ID_OFF_CHAIN_SENDER,
-    Client, Felt,
+    }, testing::ACCOUNT_ID_OFF_CHAIN_SENDER, transactions::{LocalTransactionProver, ProvingOptions}, Client, Felt
 };
 use rand::Rng;
 use uuid::Uuid;
@@ -553,6 +547,7 @@ fn create_test_client_with_store_path(store_path: &Path) -> TestClient {
     let coin_seed: [u64; 4] = rng.gen();
 
     let rng = RpoRandomCoin::new(coin_seed.map(Felt::new));
+    let tx_prover = Arc::new(LocalTransactionProver::new(ProvingOptions::default()));
 
     let authenticator = StoreAuthenticator::new_with_rng(store.clone(), rng);
     TestClient::new(
@@ -560,6 +555,7 @@ fn create_test_client_with_store_path(store_path: &Path) -> TestClient {
         rng,
         store,
         std::sync::Arc::new(authenticator),
+        tx_prover,
         true,
     )
 }
