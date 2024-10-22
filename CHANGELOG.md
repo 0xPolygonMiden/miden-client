7127d999843c072f922d1f72bb5c8b33e299237b
Julian <julian@demoxlabs.xyz>
2024-10-22 15:03:59 -0500

    fix: Error handling for web client sync state (#558)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## 0.6.0 (TBD)
 
+* Added better error handling for WASM sync state 
 * Added WASM Input note tests + updated input note models (#554)
 * Added new variants for the `NoteFilter` struct (#538).
 * [BREAKING] Added note tags for future notes in `TransactionRequest` (#538).
--- crates/web-client/src/sync.rs
+++ crates/web-client/src/sync.rs
@@ -6,7 +6,10 @@ use crate::{models::sync_summary::SyncSummary, WebClient};
 impl WebClient {
     pub async fn sync_state(&mut self) -> Result<SyncSummary, JsValue> {
         if let Some(client) = self.get_mut_inner() {
-            let sync_summary = client.sync_state().await.unwrap();
+            let sync_summary = client
+                .sync_state()
+                .await
+                .map_err(|err| JsValue::from_str(&format!("Failed to sync state: {}", err)))?;
 
             Ok(sync_summary.into())
         } else {
