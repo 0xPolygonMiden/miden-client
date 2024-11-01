3260e378a07c0ed79de022e50d85f03ee1fa70b6
Julian <julian@demoxlabs.xyz>
2024-11-01 09:54:31 -0500

    feat: Adding remote proving + improving note models (#566)
    * feat: Adding remote proving + improving note models
    
    * exposing further get apis on input note + cleanup

--- crates/web-client/src/models/input_note_record.rs
+++ crates/web-client/src/models/input_note_record.rs
@@ -1,7 +1,10 @@
 use miden_client::store::InputNoteRecord as NativeInputNoteRecord;
 use wasm_bindgen::prelude::*;
 
-use super::{input_note_state::InputNoteState, note_details::NoteDetails, note_id::NoteId};
+use super::{
+    input_note_state::InputNoteState, note_details::NoteDetails, note_id::NoteId,
+    note_inclusion_proof::NoteInclusionProof, note_metadata::NoteMetadata,
+};
 
 #[derive(Clone)]
 #[wasm_bindgen]
@@ -19,6 +22,34 @@ impl InputNoteRecord {
 
     pub fn details(&self) -> NoteDetails {
         self.0.details().into()
+    }
+
+    pub fn metadata(&self) -> Option<NoteMetadata> {
+        self.0.metadata().map(|metadata| metadata.into())
+    }
+
+    pub fn inclusion_proof(&self) -> Option<NoteInclusionProof> {
+        self.0.inclusion_proof().map(|proof| proof.into())
+    }
+
+    pub fn consumer_transaction_id(&self) -> Option<String> {
+        self.0.consumer_transaction_id().map(|id| id.to_string())
+    }
+
+    pub fn nullifier(&self) -> String {
+        self.0.nullifier().to_hex()
+    }
+
+    pub fn is_authenticated(&self) -> bool {
+        self.0.is_authenticated()
+    }
+
+    pub fn is_consumed(&self) -> bool {
+        self.0.is_consumed()
+    }
+
+    pub fn is_processing(&self) -> bool {
+        self.0.is_processing()
     }
 }
 
