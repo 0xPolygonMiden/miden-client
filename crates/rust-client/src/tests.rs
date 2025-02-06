104cfade0a5b54fc149b170ebb68f1531f0a7359
Tomas Rodriguez Dala <43424983+tomyrd@users.noreply.github.com>
2025-02-06 16:17:14 -0300

    feat: add error for empty P2ID notes (#714)
    * feat: add empty asset check for p2id notes
    
    * chore: update CHANGELOG
    
    * review: improve empty asset check

--- crates/rust-client/src/tests.rs
+++ crates/rust-client/src/tests.rs
@@ -12,7 +12,7 @@ use miden_objects::{
         Account, AccountBuilder, AccountCode, AccountHeader, AccountId, AccountStorageMode,
         AccountType, AuthSecretKey,
     },
-    asset::{FungibleAsset, TokenSymbol},
+    asset::{Asset, FungibleAsset, TokenSymbol},
     crypto::{dsa::rpo_falcon512::SecretKey, rand::FeltRng},
     note::{
         Note, NoteAssets, NoteExecutionHint, NoteExecutionMode, NoteFile, NoteMetadata, NoteTag,
@@ -34,7 +34,8 @@ use crate::{
     rpc::NodeRpcClient,
     store::{InputNoteRecord, NoteFilter, Store, StoreError},
     transaction::{
-        TransactionRequestBuilder, TransactionRequestError, TransactionScriptBuilderError,
+        PaymentTransactionData, TransactionRequestBuilder, TransactionRequestError,
+        TransactionScriptBuilderError,
     },
     Client, ClientError,
 };
@@ -689,6 +690,40 @@ async fn test_note_without_asset() {
             TransactionRequestError::TransactionScriptBuilderError(
                 TransactionScriptBuilderError::FaucetNoteWithoutAsset
             )
+        )
+    ));
+
+    let error = TransactionRequestBuilder::pay_to_id(
+        PaymentTransactionData::new(vec![], faucet.id(), wallet.id()),
+        None,
+        NoteType::Public,
+        client.rng(),
+    )
+    .unwrap_err();
+
+    assert!(matches!(
+        error,
+        TransactionRequestError::TransactionScriptBuilderError(
+            TransactionScriptBuilderError::P2IDNoteWithoutAsset
+        )
+    ));
+
+    let error = TransactionRequestBuilder::pay_to_id(
+        PaymentTransactionData::new(
+            vec![Asset::Fungible(FungibleAsset::new(faucet.id(), 0).unwrap())],
+            faucet.id(),
+            wallet.id(),
+        ),
+        None,
+        NoteType::Public,
+        client.rng(),
+    )
+    .unwrap_err();
+
+    assert!(matches!(
+        error,
+        TransactionRequestError::TransactionScriptBuilderError(
+            TransactionScriptBuilderError::P2IDNoteWithoutAsset
         )
     ));
 }
