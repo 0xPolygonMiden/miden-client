dc5142df30e30308472bd0c14e6a7f72bc29e93f
SantiagoPittella <pittellasantiago@gmail.com>
2025-03-20 11:13:53 -0300

    feat: add flag to print note code

--- bin/miden-cli/src/commands/notes.rs
+++ bin/miden-cli/src/commands/notes.rs
@@ -56,6 +56,9 @@ pub struct NotesCmd {
     /// consumable by this account will be shown.
     #[clap(short, long, value_name = "account_id")]
     account_id: Option<String>,
+    /// When using --show, include the note code in the output.
+    #[clap(long, requires = "show")]
+    with_code: bool,
 }
 
 impl NotesCmd {
@@ -72,7 +75,7 @@ impl NotesCmd {
                 .await?;
             },
             NotesCmd { show: Some(id), .. } => {
-                show_note(client, id.to_owned()).await?;
+                show_note(client, id.to_owned(), self.with_code).await?;
             },
             _ => {
                 list_notes(client, ClientNoteFilter::All).await?;
@@ -123,7 +126,11 @@ async fn list_notes(
 // SHOW NOTE
 // ================================================================================================
 #[allow(clippy::too_many_lines)]
-async fn show_note(client: Client<impl FeltRng>, note_id: String) -> Result<(), CliError> {
+async fn show_note(
+    client: Client<impl FeltRng>,
+    note_id: String,
+    with_code: bool,
+) -> Result<(), CliError> {
     let input_note_record = get_input_note_with_id_prefix(&client, &note_id).await;
     let output_note_record = get_output_note_with_id_prefix(&client, &note_id).await;
 
@@ -262,18 +269,22 @@ async fn show_note(client: Client<impl FeltRng>, note_id: String) -> Result<(), 
         println!("{table}");
     };
 
-    let mut table = create_dynamic_table(&["Note Code"]);
-    let code = match (&input_note_record, &output_note_record) {
-        (Some(record), _) => record.details().script().to_pretty_string(),
-        (_, Some(record)) => {
-            record.state().recipient().map_or("Code unavailable".to_string(), |recipient| {
-                recipient.script().to_pretty_string()
-            })
-        },
-        (None, None) => "Code unavailable".to_string(),
-    };
-    table.add_row(vec![Cell::new(code)]);
-    println!("{table}");
+    if with_code {
+        let mut table = create_dynamic_table(&["Note Code"]);
+        let code = match (&input_note_record, &output_note_record) {
+            (Some(record), _) => record.details().script().to_pretty_string(),
+            (_, Some(record)) => {
+                record.state().recipient().map_or("Code unavailable".to_string(), |recipient| {
+                    recipient.script().to_pretty_string()
+                })
+            },
+            (None, None) => {
+                panic!("One of the two records should be Some")
+            },
+        };
+        table.add_row(vec![Cell::new(code)]);
+        println!("{table}");
+    }
 
     Ok(())
 }
