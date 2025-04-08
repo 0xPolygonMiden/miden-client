d61a40cd1735eec033f919ce04e2cb26e3e758dc
tomyrd <tomas.rodriguez@lambdaclass.com>
2025-04-08 16:38:51 -0300

    refactor: `TonicRpcClient`

---
+++ crates/rust-client/src/rpc/tonic_client/api_client.rs
@@ -0,0 +1,44 @@
+#[cfg(all(not(target_arch = "wasm32"), feature = "web-tonic"))]
+compile_error!("The `web-tonic` feature is only supported when targeting wasm32.");
+
+#[cfg(feature = "web-tonic")]
+pub(crate) mod api_client_wrapper {
+    use alloc::string::String;
+
+    use crate::rpc::RpcError;
+
+    pub type ApiClient =
+        crate::rpc::generated::rpc::api_client::ApiClient<tonic_web_wasm_client::Client>;
+
+    impl ApiClient {
+        #[allow(clippy::unused_async)]
+        pub async fn new_client(endpoint: String, _timeout_ms: u64) -> Result<ApiClient, RpcError> {
+            let wasm_client = tonic_web_wasm_client::Client::new(endpoint);
+            Ok(ApiClient::new(wasm_client))
+        }
+    }
+}
+
+#[cfg(feature = "tonic")]
+pub(crate) mod api_client_wrapper {
+
+    use core::time::Duration;
+    use std::string::{String, ToString};
+
+    use crate::rpc::RpcError;
+
+    pub type ApiClient =
+        crate::rpc::generated::rpc::api_client::ApiClient<tonic::transport::Channel>;
+
+    impl ApiClient {
+        pub async fn new_client(endpoint: String, timeout_ms: u64) -> Result<ApiClient, RpcError> {
+            let endpoint = tonic::transport::Endpoint::try_from(endpoint)
+                .map_err(|err| RpcError::ConnectionError(err.to_string()))?
+                .timeout(Duration::from_millis(timeout_ms));
+
+            ApiClient::connect(endpoint)
+                .await
+                .map_err(|err| RpcError::ConnectionError(err.to_string()))
+        }
+    }
+}
