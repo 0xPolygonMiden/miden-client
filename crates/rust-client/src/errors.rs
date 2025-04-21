55a8c2c389ab2eebc58c01f8be23d0d30df2fdbd
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-04-21 11:12:16 +0300

    refactor: print enclosed error message for `ClientError::RecencyConditionError`

--- crates/rust-client/src/errors.rs
+++ crates/rust-client/src/errors.rs
@@ -68,7 +68,7 @@ pub enum ClientError {
     NoConsumableNoteForAccount(AccountId),
     #[error("rpc api error")]
     RpcError(#[from] RpcError),
-    #[error("recency condition error")]
+    #[error("recency condition error: {0}")]
     RecencyConditionError(String),
     #[error("note screener error")]
     NoteScreenerError(#[from] NoteScreenerError),
